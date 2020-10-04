9427463a4e8664a03803c16bff14558a892825a4
Ricardo <Ricardo@Poliane-PC>
2020-10-04 18:42:06 -0300

    teste

--- Publica/src/interfaceGrafica/Home.java
+++ Publica/src/interfaceGrafica/Home.java
@@ -16,7 +16,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JSeparator;
 
-//Tela principal da Aplicacao
+//Tela principal da Apli
 public class Home extends javax.swing.JFrame {
 	//Atributos privados da classe
 	private JFrame home;
